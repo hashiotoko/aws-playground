ce0e5972ede94bb1bd0e277a8c4a8afcafc95b07
hashiotoko <141216166+hashiotoko@users.noreply.github.com>
2024-10-14 19:24:54 +0900

    create dynamodb by cdktf

--- src/apigw-lambda-dynamodb/cdktf/main.ts
+++ src/apigw-lambda-dynamodb/cdktf/main.ts
@@ -1,11 +1,36 @@
 import { Construct } from 'constructs';
 import { App, TerraformStack } from 'cdktf';
+import * as aws from '@cdktf/provider-aws';
 
 class MyStack extends TerraformStack {
   constructor(scope: Construct, id: string) {
     super(scope, id);
 
     // define resources here
+    new aws.provider.AwsProvider(this, 'provider', {
+      region: 'ap-northeast-1',
+    });
+
+    const dynamodb = new aws.dynamodbTable.DynamodbTable(
+      this,
+      'test-dynamodb',
+      {
+        name: 'test_user',
+        billingMode: 'PAY_PER_REQUEST',
+        hashKey: 'UserId',
+        attribute: [{ name: 'UserId', type: 'S' }],
+      }
+    );
+
+    new aws.dynamodbTableItem.DynamodbTableItem(this, 'test-dynamodb-item', {
+      tableName: dynamodb.name,
+      hashKey: dynamodb.hashKey,
+      item: JSON.stringify({
+        UserId: { S: '1' },
+        FirstName: { S: 'Taro' },
+        LastName: { S: 'Tanaka' },
+      }),
+    });
   }
 }
 
